63d56087ea343afeb65ae40ade3971bf56a6fb8f
Tiago Marino <tmsilva11@gmail.com>
2021-03-30 12:43:39 -0300

    Remove unused classes and structs

--- Assets/Script/shared/gameConfiguration.cs
+++ Assets/Script/shared/gameConfiguration.cs
@@ -1,93 +1,4 @@
 ﻿
-using UnityEngine;
-using System;
-
-[System.Serializable]
-public class Player{
-	public string name;
-}
-
-[System.Serializable]
-public class Supervisor{
-	public string name;
-}
-/* 
-[System.Serializable]
-public class Resource{
-	public string url;
-	public string name;
-	public string license;
-	public string type;
-}
-*/
-/*
-[System.Serializable]
-public class Component{
-	public string id;
-	public string purpose;
-	public int score;
-	public Resource[] resources;
-}
-*/
-[System.Serializable]
-public class Mechanic{
-	public string description;
-	public string knowledge;
-	public Component[] components;
-}
-/*
-[System.Serializable]
-public class Game{
-	public string name;
-	public Mechanic mechanic;	
-	public Feedback[] feedbacks;
-}
-*/
-
-[System.Serializable]
-public class Feedback{
-	public string id;
-	public string message;
-}
-
-[System.Serializable]
-public class GameConfiguration{
-	public Player player;
-	public Supervisor supervisor;
-    public Game[] games;
-	
-}
-
-[System.Serializable]
-public class SessionResponse{
-	public GameConfiguration gameConfiguration;
-}
-
-[System.Serializable]
-public class GamePerformance{
-	public Player player;
-	public Supervisor supervisor;
-	public String game;
-	public DateTime date;
-	public Performance performance;
-}
-
-[System.Serializable]
-public class Performance{
-	public float time;
-	public int hits;
-    public int fails;
-	public int score;
-	public int feelingRate;
-}
-
-[System.Serializable]
-public class StopSessionRequest{
-	public GamePerformance gamePerformance;
-}
-
-/* new json */
-
 [System.Serializable]
 public class LoginRequest{
 	public string username;
@@ -127,31 +38,6 @@ public class Resource{
     public string role;
 	public int score;
 }
-/*
-[System.Serializable]
-public class Component{
-	public long id;
-	public string name;
-	public int score;
-	public string purpose;
-	public string tag;
-	public ResourceType[] resourceTypes;
-}
-*/
-[System.Serializable]
-public class GameComponent{
-	public long id;
-	public Resource[] resources;
-	public Component component;
-}
-/*
-[System.Serializable]
-public class Configuration{
-	public long id;
-	public long game;
-	public GameComponent[] gameComponents;
-}
-*/
 
 [System.Serializable]
 public class Response{
@@ -189,6 +75,6 @@ public class Game{
 
 [System.Serializable]
 public class Configuration{
-	public long id;
+	public long id; //Not used
 	public Game game;
 }
