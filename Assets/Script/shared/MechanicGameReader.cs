9f7dc521173de0e851cd60c4a8813d6898690d16
Tiago Marino <tmsilva11@gmail.com>
2021-04-13 08:23:47 -0300

    Available Mechanics Tests

---
+++ Assets/Script/shared/MechanicGameReader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public static class MechanicGameReader
+{
+    private static List<string> _availableMechanics = new List<string> { "Quizz", "Collect", "Puzzle"};
+    public static bool IsMechanicAvailable(Game game)
+    {
+        return _availableMechanics.Contains(game.mechanic_name);
+    }
+}
---
+++ Assets/Tests/EditMode Tests/MechanicReadingTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MechanicReadingTests
+    {
+        public class BaseTests
+        {
+            [Test]
+            [TestCase("Quizz")]
+            [TestCase("Collect")]
+            [TestCase("Puzzle")]
+            public void Existent_Mechanic_Available(string mechanic_name)
+            {
+                // Prepare
+                Game game = new Game();
+                game.mechanic_name = mechanic_name;
+
+                //Act
+                bool available = MechanicGameReader.IsMechanicAvailable(game);
+
+                //Assert
+                Assert.IsTrue(available, $"mechanic {mechanic_name} is not available in this project");
+            }
+
+            [Test]
+            [TestCase("Quiz")]
+            [TestCase("Platformer")]
+            [TestCase("Visual Novel")]
+            [TestCase("")]
+            [TestCase(null)]
+            public void NonExistent_Mechanic_Unavailable(string mechanic_name)
+            {
+                // Prepare
+                Game game = new Game();
+                game.mechanic_name = mechanic_name;
+
+                //Act
+                bool available = MechanicGameReader.IsMechanicAvailable(game);
+
+                //Assert
+                Assert.IsFalse(available, $"mechanic {mechanic_name} is not supposed to be available in this project");
+            }
+        }
+
+        public class QuizzTests
+        {
+        }
+    }
+}
